e2c4f3be01649e544f5a93ad4a43497387093da8
Alexandre LANDREAU <alexandre@landreau.biz>
2017-01-28 15:55:25 -0800

    first commit

---
+++ assets/app/main-aot.ts
@@ -0,0 +1,11 @@
+import './polyfills';
+
+
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModuleNgFactory } from '../../aot/assets/app/app.module.ngfactory';
+import { enableProdMode } from '@angular/core';
+
+enableProdMode();
+
+
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
