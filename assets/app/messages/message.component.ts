64ce0401b36818ec1406cc89adca11de18968160
Alexandre LANDREAU <alexandre@landreau.biz>
2017-01-31 20:05:19 -0800

    correction for jit

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from "@angular/core";
 import { Message } from "./message.model";
 import { MessageService } from "./message.service";
 
+
 @Component({
     selector: 'app-message',
     templateUrl: './message.component.html',
